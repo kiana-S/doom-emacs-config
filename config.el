f11590bd87515cc0ee518d451243c49ae4d297af
Kiana Sheibani <kiana.a.sheibani@gmail.com>
2023-01-17 21:42:16 -0500

    Switch to eglot for LSP

--- config.el
+++ config.el
@@ -222,8 +222,6 @@ See URL 'https://github.com/ProofGeneral/PG/issues/427'."
 ;; Flymake
 (add-hook! prog-mode #'flymake-mode)
 (setq-hook! flymake-mode next-error-function #'flymake-goto-next-error)
-(after! lsp-mode
-  (setq lsp-diagnostics-provider :flymake))
 
 ;; Use Flycheck's double arrow fringe indicator
 (after! flymake
