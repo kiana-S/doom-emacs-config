8df20d5da8949806cada85cae1b42fb4dd5d23e6
Kiana Sheibani <kiana.a.sheibani@gmail.com>
2023-08-31 22:29:04 -0400

    Modify emacs-everywhere module to work for sway

---
+++ modules/app/everywhere/packages.el
@@ -0,0 +1,6 @@
+;; -*- no-byte-compile: t; -*-
+;;; app/everywhere/packages.el
+
+;; Use sway-comptatible version of emacs-everywhere
+(package! emacs-everywhere
+  :recipe (:host github :repo "kiana-S/emacs-everywhere"))
